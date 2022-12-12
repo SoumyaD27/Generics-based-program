89fae2ea3114798b401dfd4e4812a72df5035e91
SoumyaD27 <soumyad233@gmail.com>
2022-12-12 22:49:04 +0530

    [Sample Code of Print Array]

---
+++ PrintArrayGenericMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintArrayGenericMethod
+{
+    class Program
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+        }
+    }
+}
