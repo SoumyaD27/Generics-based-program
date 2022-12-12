d86ae12ce0c1b6dd73ecd9a4fa5efa76955e972a
SoumyaD27 <soumyad233@gmail.com>
2022-12-12 23:19:59 +0530

    [Find Max Integer Number]

---
+++ UC1_FindIntMaxNum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UC1_FindIntMaxNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Int Number");
+       
+            int output = MaximumNumberCheck.MaximumIntNumber(11, 12, 13);
+            Console.WriteLine(output);
+        }
+    }
+}
