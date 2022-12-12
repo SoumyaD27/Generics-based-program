1bc824f005a9c9b2339dd8f2cf2ca90bd6145cc4
SoumyaD27 <soumyad233@gmail.com>
2022-12-12 23:33:53 +0530

    [Find Maximum Float Number]

---
+++ UC2_FindMaxFloatNum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UC2_FindMaxFloatNum;
+
+namespace UC2_FindMaxFloatNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Float Number");
+
+            double output = MaximumNumberCheck.MaximumFloatNumber( 3.1, 1.2, 1.3);
+            Console.WriteLine(output);
+        }
+    }
+}
