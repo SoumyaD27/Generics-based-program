eb38534eeed3605b278f11db4ece2704b7dc9cb6
SoumyaD27 <soumyad233@gmail.com>
2022-12-12 23:48:19 +0530

    [Find Maximum String Value]

---
+++ UC3_FindMaxString/MaximumNumberCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_FindMaxString
+{
+    public class MaximumNumberCheck
+    {
+        public static string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same");
+
+        }
+    }
+}
---
+++ UC3_FindMaxString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UC3_FindMaxString;
+
+namespace UC3_FindMaxStringNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max String Number");
+
+            string output = MaximumNumberCheck.MaximumStringNumber("22","33","44");
+            Console.WriteLine(output);
+        }
+    }
+}
