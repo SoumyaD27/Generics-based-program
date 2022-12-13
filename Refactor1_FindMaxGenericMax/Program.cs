661e136e993845dec86b3d1f05ec21c047463957
SoumyaD27 <soumyad233@gmail.com>
2022-12-13 13:34:37 +0530

    [Refactor to create Generic Class]

---
+++ Refactor1_FindMaxGenericMax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Refactor1_FindMaxNumGenericClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+
+            int[] intArray = { 112, 113, 444, 345 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+
+            double[] doubleArray = { 11.2, 1.13, 4.44, 34.5 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+
+            string[] stringArray = { "Apple", "Peach", "Mango"};
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
+
+        }
+    }
+}
